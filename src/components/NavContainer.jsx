b9d9223b687a56a639c9ca820fe2aeed13ad335f
josemourinho333 <yoo.phil92@gmail.com>
2022-07-29 13:44:38 -0700

    Add transparent/filled nav on scroll

--- src/components/NavContainer.jsx
+++ src/components/NavContainer.jsx
@@ -1,9 +1,24 @@
 import NavMenu from './NavMenu';
 import NavActions from './NavActions';
+import { useState, useEffect } from 'react';
 
 const NavContainer = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const scrollHandle = () => {
+    setScrollPosition(window.scrollY);
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollHandle);
+    
+    return () => {
+      window.removeEventListener('scroll', scrollHandle);
+    }
+  })
+
   return (
-    <div className="bg-transparent fixed top-0 left-0 right-0 nav-container flex justify-between px-10 py-3 ">
+    <div className={`fixed top-0 left-0 right-0 nav-container flex justify-between px-10 py-3 ${scrollPosition < 200 ? 'bg-transparent' : 'bg-indigo-500'}`}>
       <NavMenu />
       <NavActions />
     </div>
