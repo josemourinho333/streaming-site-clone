dc5109f9c2fd8b395bda7956a99ed2e4aafe70ce
josemourinho333 <yoo.phil92@gmail.com>
2022-08-25 20:12:01 -0700

    Update hero buttons

--- src/components/HeroContainer.jsx
+++ src/components/HeroContainer.jsx
@@ -1,6 +1,7 @@
 import { fetchHero } from '../movies/heroSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import addToWatchList from '../helpers/addToWatchList';
 
 const HeroContainer = () => {
   const hero = useSelector(state => state.hero.hero);
@@ -15,8 +16,12 @@ const HeroContainer = () => {
       <h1 className='hero-title text-4xl font-bold mb-4'>{hero.title}</h1>
       <p className="hero-desc font-medium text-md mb-4">{hero.overview}</p>
       <div className="flex hero-button-wrapper">
-        <button className="hero-button cursor-pointer font-bold rounded px-8 py-2 mr-2 text-black bg-white hover:bg-[#e6e6e6] transition-all">Button</button>
-        <button className="hero-button cursor-pointer font-bold rounded px-8 py-2 mr-2 text-black bg-white hover:bg-[#e6e6e6] transition-all">+ Add to My List</button>
+        <button className="hero-button cursor-pointer font-bold rounded px-8 py-2 mr-2 text-black bg-white hover:bg-[#e6e6e6] transition-all">
+          <a href={`/${hero.title ? 'movie' : 'tv'}/${hero.id}/${hero.title ? hero.title : hero.name}`}>
+          Learn more
+          </a>
+        </button>
+        <button onClick={() => addToWatchList(hero)} className="hero-button cursor-pointer font-bold rounded px-8 py-2 mr-2 text-black bg-white hover:bg-[#e6e6e6] transition-all">+ Add to Watchlist</button>
       </div>
     </div>
   )
